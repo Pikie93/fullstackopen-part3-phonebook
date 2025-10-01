c7f8e36b21e6fdb6f7d6879325155003c2650724
Pikie93 <jamesdown12@gmail.com>
2025-10-01 15:52:57 +0200

    hopefully fixed render error

--- phonebookback/index.js
+++ phonebookback/index.js
@@ -116,7 +116,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(express.static("dist"));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "dist", "index.html"));
 });
 
